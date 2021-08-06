8ff6cfd8ba211b346df44116bcef6bc7496d7bf2
israpps <isr_apps_help@hotmail.com>
2021-08-06 18:01:16 -0300

    v1.0.0 - Revision 1

--- version.h
+++ version.h
@@ -17,13 +17,13 @@ namespace AutoVersion{
 	static const long MAJOR  = 1;
 	static const long MINOR  = 0;
 	static const long BUILD  = 0;
-	static const long REVISION  = 0;
+	static const long REVISION  = 1;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 2;
-	#define RC_FILEVERSION 1,0,0,0
-	#define RC_FILEVERSION_STRING "1, 0, 0, 0\0"
-	static const char FULLVERSION_STRING [] = "1.0.0.0";
+	static const long BUILDS_COUNT  = 0;
+	#define RC_FILEVERSION 1,0,0,1
+	#define RC_FILEVERSION_STRING "1, 0, 0, 1\0"
+	static const char FULLVERSION_STRING [] = "1.0.0.1";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY  = 0;
